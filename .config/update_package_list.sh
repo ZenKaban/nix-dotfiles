3b20873bbc16c321ba9822fbf65c91991bce848a
alex <kabanoff90@gmail.com>
2023-12-31 20:04:15 +0300

    fixes

--- .config/update_package_list.sh
+++ .config/update_package_list.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-/usr/bin/pacman -Qem | cut -f 1 -d " " > ~/.config/aur_packages
-/usr/bin/pacman -Qe | cut -f 1 -d " " > ~/.config/pacman_packages
+/usr/bin/pacman -Qem | cut -f 1 -d " " > /home/alex/.config/aur_packages
+/usr/bin/pacman -Qe | cut -f 1 -d " " > /home/alex/.config/pacman_packages
 
