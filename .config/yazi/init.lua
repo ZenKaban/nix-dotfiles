62d45b2654da471d5d7d2898f1255d73237642e2
kabanov-alex <kabanoff90@gmail.com>
2024-12-09 11:51:41 +0300

    updated yazi

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -2,9 +2,9 @@ require("zoxide"):setup {
 	update_db = true,
 }
 
-require("full-border"):setup {
-	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-	type = ui.Border.ROUNDED,
-}
-
-require("git"):setup()
+-- require("full-border"):setup {
+-- 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+-- 	type = ui.Border.ROUNDED,
+-- }
+--
+-- require("git"):setup()
