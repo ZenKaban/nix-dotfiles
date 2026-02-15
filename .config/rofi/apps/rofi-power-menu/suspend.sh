d8724200cd897d897cd09fd1140871e12fc816e7
ZenKaban <email@email.com>
2026-02-16 00:00:53 +0300

    a

--- .config/rofi/apps/rofi-power-menu/suspend.sh
+++ .config/rofi/apps/rofi-power-menu/suspend.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-if swaymsg "input type:keyboard xkb_switch_layout 0" && swaylock --daemonize && systemctl suspend ; 
-# if hyprlock && systemctl suspend ; 
+# if swaymsg "input type:keyboard xkb_switch_layout 0" && swaylock --daemonize && systemctl suspend ; 
+if hyprlock && systemctl suspend ; 
 then 
     ''    
 else
